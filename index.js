016a44bdd62aa8b94123584b3c886cd70426259c
Hana Pavlova <hanapavlova7@gmail.com>
2024-03-06 11:11:01 +0100

    první pokus

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('funguju!');
+/*Doplňte do stránky JavaScriptový program, který pomocí funkce prompt požádá uživatele aby vyjářil svoji úroveň postupně ve všech zobrazených dovednostech jako číslo 0–100. Program pak nastaví posuvníky na stránce na odpovídající délku a hodnoty u posuvníků na uživatelem zadaná čísla.
+Na samém začátku vašeho programu vytvořte funkci updateSkill, která bere dva vstupy: textový řetězec představující id elementu a číslo mezi 0–100. Tato funkce při zavolání nastaví posuvník se zadaným id na správnou hodnotu. Použijte tuto funkci ve vašem kódu, abyste nemuseli při nastavování posuvníků psát třikrát po sobě téměř stejný kód.*/
+
+const urovenHTML = Number(prompt("Ohodnoťte svoje dovednosti v HTML. Zadejte svoji úroveň v bodech 0-100."))
+const urovenCSS = Number(prompt("Ohodnoťte svoje dovednosti v CSS. Zadejte svoji úroveň v bodech 0-100."))
+const urovenJS = Number(prompt("Ohodnoťte svoje dovednosti v JS. Zadejte svoji úroveň v bodech 0-100."))
+
+const updateSkill = (id, uroven) => {
+  document.querySelector("#id")
+  const value = document.querySelector(".skill__value")
+}
+
+if (urovenHTML >=0 && urovenHTML <=100) { 
+updateSkill("skill1", urovenHTML)
+const progressHTML = document.querySelector("#skill1 .skill__progress")
+progressHTML.style.width = urovenHTML + '%';
+const valueHTML = document.querySelector("#skill1 .skill__value")
+valueHTML.textContent = urovenHTML + "/ 100";
+
+  if (urovenCSS >=0 && urovenCSS <=100)
+  {updateSkill("skill2", urovenCSS)
+  const progressCSS = document.querySelector("#skill2 .skill__progress")
+  progressCSS.style.width = urovenCSS + '%';
+  const valueCSS = document.querySelector("#skill2 .skill__value")
+  valueCSS.textContent = urovenCSS + "/ 100";
+
+    if (urovenJS >=0 && urovenJS <=100) {
+      updateSkill("skill3", urovenJS)
+      const progressJS = document.querySelector("#skill3 .skill__progress")
+      progressJS.style.width = urovenJS + '%';
+      const valueJS = document.querySelector("#skill3 .skill__value")
+      valueJS.textContent = urovenJS + "/ 100";
+
+    } else {
+      document.body.innerHTML += "Zadané hodnoty nejsou v rozmezí 0-100. Prosím, zadejte platné hodnoty."
+  } 
+} else {
+  document.body.innerHTML += "Zadané hodnoty nejsou v rozmezí 0-100. Prosím, zadejte platné hodnoty."
+} 
+} else {
+  document.body.innerHTML += "Zadané hodnoty nejsou v rozmezí 0-100. Prosím, zadejte platné hodnoty."
+}
